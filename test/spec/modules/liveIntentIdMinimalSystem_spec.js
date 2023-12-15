845fbca1710298995405c66a555b105909ba71ab
wiem <welabidine@liveintent.com>
2023-12-15 11:08:02 +0100

    fix typo

--- test/spec/modules/liveIntentIdMinimalSystem_spec.js
+++ test/spec/modules/liveIntentIdMinimalSystem_spec.js
@@ -241,7 +241,7 @@ describe('LiveIntentMinimalId', function() {
     expect(callBackSpy.calledOnce).to.be.true;
   });
 
-  it('should decode a uid2 to a seperate object when present', function() {
+  it('should decode a uid2 to a separate object when present', function() {
     const result = liveIntentIdSubmodule.decode({ nonId: 'foo', uid2: 'bar' });
     expect(result).to.eql({'lipb': {'lipbid': 'foo', 'nonId': 'foo', 'uid2': 'bar'}, 'uid2': {'id': 'bar'}});
   });
@@ -251,22 +251,22 @@ describe('LiveIntentMinimalId', function() {
     expect(result).to.eql({'uid2': {'id': 'bar'}});
   });
 
-  it('should decode a magnite id to a seperate object when present', function() {
+  it('should decode a magnite id to a separate object when present', function() {
     const result = liveIntentIdSubmodule.decode({ nonId: 'foo', magnite: 'bar' });
     expect(result).to.eql({'lipb': {'lipbid': 'foo', 'nonId': 'foo', 'magnite': 'bar'}, 'magnite': {'id': 'bar'}});
   });
 
-  it('should decode an index id to a seperate object when present', function() {
+  it('should decode an index id to a separate object when present', function() {
     const result = liveIntentIdSubmodule.decode({ nonId: 'foo', index: 'bar' });
     expect(result).to.eql({'lipb': {'lipbid': 'foo', 'nonId': 'foo', 'index': 'bar'}, 'index': {'id': 'bar'}});
   });
 
-  it('should decode a sovrn id to a seperate object when present', function() {
+  it('should decode a sovrn id to a separate object when present', function() {
     const result = liveIntentIdSubmodule.decode({ nonId: 'foo', sovrn: 'bar' });
     expect(result).to.eql({'lipb': {'lipbid': 'foo', 'nonId': 'foo', 'sovrn': 'bar'}, 'sovrn': {'id': 'bar'}});
   });
 
-  it('should decode a thetradedesk id to a seperate object when present', function() {
+  it('should decode a thetradedesk id to a separate object when present', function() {
     const result = liveIntentIdSubmodule.decode({ nonId: 'foo', thetradedesk: 'bar' });
     expect(result).to.eql({'lipb': {'lipbid': 'foo', 'nonId': 'foo', 'thetradedesk': 'bar'}, 'thetradedesk': {'id': 'bar'}});
   });
